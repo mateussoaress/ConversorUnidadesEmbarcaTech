94e764cb36244895dcc5241929899bf4fb6a9f6c
GabrielMenezesCarvalho <gabmenezes17@gmail.com>
2024-12-26 21:05:32 -0300

    Adicionada a função converterArmazenamento. Foi transformado tudo que era float em double, inclusive o dos outros integrantes.

--- ConversorUnidades.c
+++ ConversorUnidades.c
@@ -2,49 +2,64 @@
 
 // Declaraçao das funçoes de conversao
 
-float converterVolume(float valor, char unidadeOrigem, char unidadeDestino);     // Mateus Soares
-float converter_compri(float, char , char );  //Matheus Capuchinho
-float converterMassa(float valor, char unidadeOrigem, char unidadeDestino);    // Gabriel Oliveira
+double converterVolume(double valor, char unidadeOrigem, char unidadeDestino);        // Mateus Soares
+double converter_compri(double, char, char);                                          // Matheus Capuchinho
+double converterMassa(double valor, char unidadeOrigem, char unidadeDestino);         // Gabriel Oliveira
+double converterArmazenamento(double valor, char unidadeOrigem, char unidadeDestino); // Gabriel Menezes
 
 // Funçao para exibir o menu principal
 void exibirMenu();
 
-int main() {
+int main()
+{
     int escolha;
-    float valor, resultado;
+    double valor, resultado;
     char unidadeOrigem, unidadeDestino;
 
-    while (1) {
+    while (1)
+    {
         exibirMenu();
         printf("Escolha uma opcao (ou 0 para sair): ");
         scanf("%d", &escolha);
 
-        if (escolha == 0) {
+        if (escolha == 0)
+        {
             printf("Programa encerrado.\n");
             break;
         }
 
         printf("Digite o valor a ser convertido: ");
-        scanf("%f", &valor);
+        scanf("%lf", &valor);
         printf("Digite a unidade de origem: ");
         scanf(" %c", &unidadeOrigem);
         printf("Digite a unidade de destino: ");
         scanf(" %c", &unidadeDestino);
 
-        switch (escolha) {
-            case 1:
-                resultado = converterVolume(valor, unidadeOrigem, unidadeDestino); // Exemplo
-                break;
-            case 2: 
-                resultado = converterMassa(valor, unidadeOrigem, unidadeDestino);
-                break;
+        switch (escolha)
+        {
+        case 1:
+            resultado = converterVolume(valor, unidadeOrigem, unidadeDestino); 
+            break;
+        case 2:
+            resultado = converterMassa(valor, unidadeOrigem, unidadeDestino);
+            break;
 
-            case 3:
-                resultado = converter_compri(valor, unidadeOrigem, unidadeDestino);
-                break;
-            default:
-                printf("Opcao invalida. Tente novamente.\n");
-                continue;
+        case 3:
+            resultado = converter_compri(valor, unidadeOrigem, unidadeDestino);
+            break;
+        case 4:
+            resultado = converterArmazenamento(valor, unidadeOrigem, unidadeDestino);
+            if (resultado != -1)
+            {
+                if (resultado < 1e-10)
+                    printf("Resultado da conversao: %.10e\n", resultado);
+                else
+                    printf("Resultado da conversao: %.10f\n", resultado);
+            }
+            break;
+        default:
+            printf("Opcao invalida. Tente novamente.\n");
+            continue;
         }
 
         printf("Resultado da conversao: %.2f\n", resultado);
@@ -53,48 +68,78 @@ int main() {
     return 0;
 }
 
-void exibirMenu() {
+void exibirMenu()
+{
     printf("\n--- Conversor de Unidades ---\n");
     printf("1. Volume (L, mL, m3)\n");
-    printf("2. Massa Kg(q), T(t), g(g) ");
+    printf("2.  Kg(q), T(t), g(g) ");
     printf("3. Comprimento (m, c(cm), l(mm))\n");
+    printf("4. Armazenamento (b, B, K, M, G, T)\n");
 }
 
 // Funçao implementada: Volume
-float converterVolume(float valor, char unidadeOrigem, char unidadeDestino) {
-    if (unidadeOrigem == 'L' && unidadeDestino == 'm') { // Litros para mililitros
+double converterVolume(double valor, char unidadeOrigem, char unidadeDestino)
+{
+    if (unidadeOrigem == 'L' && unidadeDestino == 'm')
+    { // Litros para mililitros
         return valor * 1000;
-    } else if (unidadeOrigem == 'm' && unidadeDestino == 'L') { // Mililitros para litros
+    }
+    else if (unidadeOrigem == 'm' && unidadeDestino == 'L')
+    { // Mililitros para litros
         return valor / 1000;
-    } else if (unidadeOrigem == 'L' && unidadeDestino == 'c') { // Litros para metros cubicos
+    }
+    else if (unidadeOrigem == 'L' && unidadeDestino == 'c')
+    { // Litros para metros cubicos
         return valor / 1000;
-    } else if (unidadeOrigem == 'c' && unidadeDestino == 'L') { // Metros cubicos para litros
+    }
+    else if (unidadeOrigem == 'c' && unidadeDestino == 'L')
+    { // Metros cubicos para litros
         return valor * 1000;
-    } else if (unidadeOrigem == 'm' && unidadeDestino == 'c') { // Mililitros para metros cubicos
+    }
+    else if (unidadeOrigem == 'm' && unidadeDestino == 'c')
+    { // Mililitros para metros cubicos
         return valor / 1000000;
-    } else if (unidadeOrigem == 'c' && unidadeDestino == 'm') { // Metros cubicos para mililitros
+    }
+    else if (unidadeOrigem == 'c' && unidadeDestino == 'm')
+    { // Metros cubicos para mililitros
         return valor * 1000000;
-    } else {
+    }
+    else
+    {
         printf("Conversao invalida. Unidades nao reconhecidas.\n");
         return valor; // Retorna o mesmo valor se a conversao nao for valida
     }
 }
 
-//Funçao para converter Comprimento
-float converter_compri(float valor, char unidadeOrigem, char unidadeDestino) {
-    if (unidadeOrigem == 'm' && unidadeDestino == 'c') { // Metro para Centimetro
+// Funçao para converter Comprimento
+double converter_compri(double valor, char unidadeOrigem, char unidadeDestino)
+{
+    if (unidadeOrigem == 'm' && unidadeDestino == 'c')
+    { // Metro para Centimetro
         return valor * 100;
-    } else if (unidadeOrigem == 'm' && unidadeDestino == 'l') { // Metro para Milimetro
+    }
+    else if (unidadeOrigem == 'm' && unidadeDestino == 'l')
+    { // Metro para Milimetro
         return valor * 1000;
-    } else if (unidadeOrigem == 'c' && unidadeDestino == 'l') { // Centimetro para Milimetro
+    }
+    else if (unidadeOrigem == 'c' && unidadeDestino == 'l')
+    { // Centimetro para Milimetro
         return valor * 10;
-    } else if (unidadeOrigem == 'c' && unidadeDestino == 'm') { // Centimetro para Metro
+    }
+    else if (unidadeOrigem == 'c' && unidadeDestino == 'm')
+    { // Centimetro para Metro
         return valor / 100;
-    } else if (unidadeOrigem == 'l' && unidadeDestino == 'm') { // Milimetro para Metro
+    }
+    else if (unidadeOrigem == 'l' && unidadeDestino == 'm')
+    { // Milimetro para Metro
         return valor / 1000;
-    } else if (unidadeOrigem == 'l' && unidadeDestino == 'c') { // Milimetro para Centimetro
+    }
+    else if (unidadeOrigem == 'l' && unidadeDestino == 'c')
+    { // Milimetro para Centimetro
         return valor / 10;
-    } else {
+    }
+    else
+    {
         printf("Conversao invalida. Unidades nao reconhecidas.\n");
         return valor; // Retorna o mesmo valor se a conversao nao for valida
     }
@@ -102,30 +147,85 @@ float converter_compri(float valor, char unidadeOrigem, char unidadeDestino) {
 
 // Função implementada: Massa
 
-float converterMassa(float valor, char unidadeOrigem, char unidadeDestino){
+double converterMassa(double valor, char unidadeOrigem, char unidadeDestino)
+{
 
-    if (unidadeOrigem == 'q' && unidadeDestino == 'g'){ // Quilos para gramas
-       return valor * 1000;
+    if (unidadeOrigem == 'q' && unidadeDestino == 'g')
+    { // Quilos para gramas
+        return valor * 1000;
     }
-    else if (unidadeOrigem == 'g' && unidadeDestino == 'q'){ // gramas para quilos
-       return valor/1000;
+    else if (unidadeOrigem == 'g' && unidadeDestino == 'q')
+    { // gramas para quilos
+        return valor / 1000;
     }
-    else if (unidadeOrigem == 'q' && unidadeDestino == 't'){ // quilos para toneladas
-       return valor/1000;
+    else if (unidadeOrigem == 'q' && unidadeDestino == 't')
+    { // quilos para toneladas
+        return valor / 1000;
     }
-    else if (unidadeOrigem == 'g' && unidadeDestino == 't'){ // gramas para toneladas
-       return valor/100000;
+    else if (unidadeOrigem == 'g' && unidadeDestino == 't')
+    { // gramas para toneladas
+        return valor / 100000;
     }
-    else if (unidadeOrigem == 't' && unidadeDestino == 'g'){ // toneladas para gramas
-       return valor*1000000;
+    else if (unidadeOrigem == 't' && unidadeDestino == 'g')
+    { // toneladas para gramas
+        return valor * 1000000;
     }
-    else if (unidadeOrigem == 't' && unidadeDestino == 'q'){ // toneladas para quilos
-        return valor*1000;
+    else if (unidadeOrigem == 't' && unidadeDestino == 'q')
+    { // toneladas para quilos
+        return valor * 1000;
     }
-     else {
+    else
+    {
         printf("Conversao invalida. Unidades nao reconhecidas.\n");
         return valor; // Retorna o mesmo valor se a conversão não for válida
     }
 }
 
-// Funçoes a serem implementadas:
+// Função implementada: Armazenamento
+double converterArmazenamento(double valor, char unidadeOrigem, char unidadeDestino)
+{
+    // Conversão direta sem passar por bits para evitar precisão excessiva ou overflow
+    const double factor = 1024.0; // Fator de conversão entre unidades
+
+    switch (unidadeOrigem)
+    {
+    case 'b': case 'B': // Bits e Bytes são tratados como entrada direta
+        break;
+
+    case 'K':
+        valor *= factor;
+        break;
+    case 'M':
+        valor *= factor * factor;
+        break;
+    case 'G':
+        valor *= factor * factor * factor;
+        break;
+    case 'T':
+        valor *= factor * factor * factor * factor;
+        break;
+    default:
+        printf("Unidade de entrada invalida.\n");
+        return -1;
+    }
+
+    // Conversão para a unidade de destino
+    switch (unidadeDestino)
+    {
+    case 'b':
+        return valor * 8; // Byte para bit
+    case 'B':
+        return valor;
+    case 'K':
+        return valor / factor;
+    case 'M':
+        return valor / (factor * factor);
+    case 'G':
+        return valor / (factor * factor * factor);
+    case 'T':
+        return valor / (factor * factor * factor * factor);
+    default:
+        printf("Unidade de destino invalida.\n");
+        return -1;
+    }
+}
